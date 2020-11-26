270cc9fdd834829203f3f6dd36d8619d6b658637
Md. Abdul Bari <abdulbari@iut-dhaka.edu>
2020-11-27 02:11:16 +0600

    App created and Authentication, basic Navigation added

---
+++ blogApp/src/screens/HomeScreen.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { ScrollView, View, StyleSheet, FlatList } from "react-native";
+import { Card, Button, Text, Avatar, Input } from "react-native-elements";
+import PostCard from "./../components/PostCard";
+import HeaderHome from "./../components/Header";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import { AuthContext } from "../providers/AuthProvider";
+
+const HomeScreen = (props) => {
+  return (
+    <AuthContext.Consumer>
+      {(auth) => (
+        <View style={styles.viewStyle}>
+          <HeaderHome
+            DrawerFunction={() => {
+              props.navigation.toggleDrawer();
+            }}
+          />
+          <Card>
+            <Input
+              placeholder="What's On Your Mind?"
+              leftIcon={<Entypo name="pencil" size={24} color="black" />}
+            />
+            <Button title="Post" type="outline" onPress={function () { }} />
+          </Card>
+
+          <PostCard
+            author="EfazZ"
+            title="Hello World"
+            body="This is my First Post!"
+          />
+        </View>
+      )}
+    </AuthContext.Consumer>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+    color: "blue",
+  },
+  viewStyle: {
+    flex: 1,
+  },
+});
+
+export default HomeScreen;
